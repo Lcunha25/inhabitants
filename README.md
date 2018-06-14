b794834ebb6f028960d96e2c7f22a32975d1a8eb
Lucas <lucaspereiracunha08@gmail.com>
2018-06-13 18:24:33 -0700

    commit read me and some alterations from wp-content folder

---
+++ README.md
@@ -0,0 +1,19 @@
+# Inhabitent Project
+## This is our inhabitent project for RED academy. It is a mobile-first website made on wordpress with extra custom plug-ins created for multiple uses. 
+### The website is built with Wordpress, MySql, PHP, SCSS, CSS, HTML, Gulp and Javascript (Jquery).
+
+# Loops
+### The website is responsive to changes on the MySql database updates. The loops used on the shop, journal and blogpost tags will automatically catch updates made and fetch them to the page. Multiple loops have been used for specific porpuses.
+
+# Fonts
+### This project is done by downoloading the founts we are using from google founts and appending it to the project using font-face.
+
+# Structure main files:
+    index.html
+    style.min.css - min file of multiple sass archives
+    main.min.js - min file of main.js
+    enqueu.php - file enqueuing important scripts.
+    loop-jornal - File controling the loops on the page. Imported on specific php files to use the loop.
+
+# License
+This project is licensed under the MIT License
--- plugins/red-functionality-master/lib/functions/post-types.php
+++ plugins/red-functionality-master/lib/functions/post-types.php
@@ -59,3 +59,57 @@ function product_post_type() {
 
 }
 add_action( 'init', 'product_post_type', 0 );
+
+function adventures_post_type() {
+
+	$labels = array(
+		'name'                  => _x( 'Adventures', 'Post Type General Name', 'inhabitent' ),
+		'singular_name'         => _x( 'Adventures', 'Post Type Singular Name', 'inhabitent' ),
+		'menu_name'             => __( 'Adventures', 'inhabitent' ),
+		'name_admin_bar'        => __( 'Post', 'inhabitent' ),
+		'archives'              => __( 'Adventures Archives', 'inhabitent' ),
+		'attributes'            => __( 'Adventures Attributes', 'inhabitent' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'inhabitent' ),
+		'all_items'             => __( 'All Items', 'inhabitent' ),
+		'add_new_item'          => __( 'Add New Item', 'inhabitent' ),
+		'add_new'               => __( 'Add New', 'inhabitent' ),
+		'new_item'              => __( 'New Item', 'inhabitent' ),
+		'edit_item'             => __( 'Edit Item', 'inhabitent' ),
+		'update_item'           => __( 'Update Item', 'inhabitent' ),
+		'view_item'             => __( 'View Item', 'inhabitent' ),
+		'view_items'            => __( 'View Items', 'inhabitent' ),
+		'search_items'          => __( 'Search Item', 'inhabitent' ),
+		'not_found'             => __( 'Not found', 'inhabitent' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'inhabitent' ),
+		'featured_image'        => __( 'Featured Image', 'inhabitent' ),
+		'set_featured_image'    => __( 'Set featured image', 'inhabitent' ),
+		'remove_featured_image' => __( 'Remove featured image', 'inhabitent' ),
+		'use_featured_image'    => __( 'Use as featured image', 'inhabitent' ),
+		'insert_into_item'      => __( 'Insert into item', 'inhabitent' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'inhabitent' ),
+		'items_list'            => __( 'Adventures list', 'inhabitent' ),
+		'items_list_navigation' => __( 'Adventures list navigation', 'inhabitent' ),
+		'filter_items_list'     => __( 'Filter items list', 'inhabitent' ),
+	);
+	$args = array(
+		'label'                 => __( 'Adventures', 'inhabitent' ),
+		'description'           => __( 'This is a custom post type for our Adventures', 'inhabitent' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'Adventures', $args );
+
+}
+add_action( 'init', 'adventures_post_type', 0 );
